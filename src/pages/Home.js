35d31420856005036046ee32708badb4b40c3e52
Parthasarathy B <pbakthavatsalam@ctrl.nl>
2020-01-25 17:19:10 +0100

    link services component to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,17 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
+import Services from '../components/Services'
 
 export default function Home() {
   return (
+    <>
     <Hero>
       <Banner title="Luxurious rooms" subtitle="deluxe rooms starting at $299">
         <Link className="btn-primary" to="/rooms">Our Rooms</Link>
       </Banner>
     </Hero>
+    <Services />
+    </>
   )
 }
