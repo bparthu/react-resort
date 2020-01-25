b19a9156c972275ed89ad5aac7064b06bb8a9048
Parthasarathy B <pbakthavatsalam@ctrl.nl>
2020-01-25 17:02:24 +0100

    add banner to error hero

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
 
 export default function Error() {
   return (
-    <Hero />
+    <Hero>
+      <Banner title="404" subtitle="Page not found">
+        <Link className="btn-primary" to="/">Back to home</Link>
+      </Banner>
+    </Hero>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
-import {Link} from 'react-router-dom'
 
 export default function Home() {
   return (
